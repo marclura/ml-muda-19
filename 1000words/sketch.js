fe7d79e32ee39dc5537e9e403db27c658100a6d6
Marco Lurati <marco@marcolurati.ch>
2019-07-26 13:59:32 +0200

    first commit

---
+++ 1000words/sketch.js
@@ -0,0 +1,79 @@
+
+let img;
+let imageReady = false;
+let modelReady = false;
+let labelsReady = false;
+let textReady = false;
+let txt;
+let input;
+let sentences;
+
+let classifier, charRNN;
+let labels;
+
+
+function setup() {
+  createCanvas(400, 400);
+  img = loadImage("holidays.jpg", onImageReady); // callback
+}
+
+function onImageReady() {
+  classifier = ml5.imageClassifier('MobileNet', 'topk: 3', modelLoaded); // load classifier
+  imageReady = true;
+  charRNN = ml5.charRNN('./models/woolf/', modelTextLoaded);
+  
+}
+
+function modelLoaded() {
+  classifier.classify(img, onResult);
+}
+
+function onResult(error, result) {
+  let labelstring = "";
+  result.forEach(function(e) {    // getting all the results
+    labelstring += (labelstring == "") ? e.label : ", " + e.label;    // combine them in a sting spearated by comma
+  });
+  labels = labelstring.split(", ");  // create the array
+  console.log(labels);
+  
+  labelsReady = true;
+
+  generateText();
+}
+
+
+
+function modelTextLoaded() {
+  modelReady = true;
+}
+
+function generateText() {
+  
+  input = "the " + labels[1];  // get the firs label from the label array
+  //input = "the meaning of life is";
+  
+  console.log(input);
+
+  let data = {
+    seed: input,
+    temperature: 0.5,
+    length: 400
+  };
+  
+  charRNN.generate(data, gotData);
+  
+}
+
+function gotData(err, result) {
+    txt = input + " " + result.sample;
+    console.log(txt);
+    sentences = txt.split('.');
+    textReady = true;
+    select('#sentence').html(sentences[0] + ".");
+}
+
+function draw() {
+  if (imageReady) {
+    image(img, 0, 0, width, height/2);
+  }
+}
---
+++ 1000words/sketch_google_vision_api.js
@@ -0,0 +1,80 @@
+let img;
+let imageReady = false;
+
+function setup() {
+  createCanvas(400, 400);
+
+  img = loadImage("holidays.jpg", onImageReady); // callback
+}
+
+function onImageReady() {
+  imageReady = true;
+
+}
+
+function blobToBase64(blob, cb) {
+  var reader = new window.FileReader();
+  reader.readAsDataURL(blob);
+  reader.onloadend = function() {
+    cb(reader.result);
+  }
+}
+
+function canvasToBase64(canvas, cb) {
+  canvas.toBlob(function(blob) {
+    blobToBase64(blob, cb);
+  }, 'image/jpeg');
+}
+
+
+
+function draw() {
+  background(0);
+
+  if (imageReady) {
+    image(img, 0, 0, width, height);
+  }
+}
+
+function upload() {
+  canvasToBase64(canvas, function(b64) {
+    b64 = b64.replace('data:image/jpeg;base64,', '');
+    request = {
+      "requests": [
+        {
+          "image":{
+            "content": b64
+          },
+          "features": [
+            {
+              "type": "LABEL_DETECTION",
+              "maxResults": 5
+            }
+          ]
+        }
+      ]
+    };
+    
+    $.ajax({
+      
+      method: 'POST',
+      url: 'https://vision.googleapis.com/v1/images:annotate?key=<API-KEY>',
+      contentType: 'application/json',
+      data: JSON.stringify(request),
+      processData: false,
+      success: function(data) {
+        output = data;
+        var labels = data.responses[0].labelAnnotations[0];
+        console.log('labels: ' + labels[0].description);
+      },
+      error: function (data, textStatus, errorThrown) {
+        console.log('error: ' + data);
+      }
+    })
+    
+  })
+}
+
+function mousePressed() {
+  upload();
+}
